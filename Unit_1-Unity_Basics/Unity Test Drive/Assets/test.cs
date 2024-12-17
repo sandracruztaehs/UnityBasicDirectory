def5e0b0b50f2859d5979df45a037d75065a95bb
sandracruztaehs <121110657+sandracruztaehs@users.noreply.github.com>
2024-12-17 10:03:17 -0500

    Update test.cs

--- Unit_1-Unity_Basics/Unity Test Drive/Assets/test.cs
+++ Unit_1-Unity_Basics/Unity Test Drive/Assets/test.cs
@@ -5,7 +5,7 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Luna is great
     }
 
     // Update is called once per frame
